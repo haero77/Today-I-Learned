e345a8cd1f45017e9faaf4b91a81bafdd684b49e
preferkim <shineecard5@gmail.com>
2022-10-12 21:09:59 +0900

    add: 모던 자바 인 액션 / ch05 스트림 활용 / 5.6 실전 연습

---
+++ sample_code/modern_java_in_action/ch04_06_stream/challenge/TransactionData.java
@@ -0,0 +1,20 @@
+package sample_code.modern_java_in_action.ch04_06_stream.challenge;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TransactionData {
+    Trader raoul = new Trader("Raoul", "Cambridge");
+    Trader mario = new Trader("Mario", "Milan");
+    Trader alan = new Trader("Alan", "Cambridge");
+    Trader brian = new Trader("Brian", "Cambridge");
+
+    List<Transaction> transactions = Arrays.asList(
+            new Transaction(brian, 2011, 300),
+            new Transaction(raoul, 2012, 1000),
+            new Transaction(raoul, 2011, 400),
+            new Transaction(mario, 2012,  710),
+            new Transaction(mario, 2012,  700),
+            new Transaction(alan, 2012, 950)
+    );
+}
