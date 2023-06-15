e28bb4e8aa0e74a7ce09dd9648fdcff85124bc0f
Seonho Kim <sunhowold@naver.com>
2023-06-15 17:06:44 +0900

    move Application Class to directory

--- _05_Test_Code/projects/practical_test/src/main/java/sample/cafekiosk/CafekioskApplication.java
+++ _05_Test_Code/projects/practical_test/src/main/java/sample/cafekiosk/CafekioskApplication.java
@@ -1,4 +1,4 @@
-package sample.cafekiosk.spring;
+package sample.cafekiosk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
