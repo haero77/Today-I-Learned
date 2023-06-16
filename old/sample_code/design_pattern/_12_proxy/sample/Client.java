5968006e7ef13439365a2806830891d06c33bfb7
Seonho Kim <sunhowold@naver.com>
2023-06-16 15:24:54 +0900

    [Network] HTTP Keep Alive 옵션 (HTTP 1.1부터 default 옵션)

--- old/sample_code/design_pattern/_12_proxy/sample/Client.java
+++ old/sample_code/design_pattern/_12_proxy/sample/Client.java
@@ -5,7 +5,7 @@ import sample_code.design_pattern._12_proxy.sample.DefaultGameService;
 import sample_code.design_pattern._12_proxy.sample.GameService;
 import sample_code.design_pattern._12_proxy.sample.GameServiceProxy;
 
-public class Client {
+pub0lic class Client {
 
     public static void main(String[] args) {
         GameService gameService = new GameServiceProxy(new DefaultGameService());
