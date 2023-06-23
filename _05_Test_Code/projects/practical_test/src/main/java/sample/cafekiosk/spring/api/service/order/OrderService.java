90dda86ca9b540cfb8cb1f524293ac77e76b0287
Seonho Kim <sunhowold@naver.com>
2023-06-23 17:45:58 +0900

    [inf-testcode] Business Layer 테스트: 중복 상품 번호 주문 생성 가능 (GREEN)

--- _05_Test_Code/projects/practical_test/src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
+++ _05_Test_Code/projects/practical_test/src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -2,6 +2,8 @@ package sample.cafekiosk.spring.api.service.order;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -21,12 +23,22 @@ public class OrderService {
 	private final OrderRepository orderRepository;
 
 	public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDateTime) {
-		List<String> productNumbers =  request.getProductNumbers();
+		List<String> productNumbers = request.getProductNumbers();
 
+		// 중복이 제거된 프로덕트가 조회된다.
 		List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
 
+		// productNumber를 기반으로 Product를 빨리 찾을 수 있는 Map을 만든다.
+		Map<String, Product> productMap = products.stream()
+			.collect(Collectors.toMap(Product::getProductNumber, product -> product));
+
+		// productNumber -> Product 변환
+		List<Product> duplicateProducts = productNumbers.stream()
+			.map(productMap::get)
+			.collect(Collectors.toList());
+
 		// LocalDateTime.now()가 서비스 레이어에 있으면 테스트 하기 어려우므로 파라미터로 추출
-		Order order = Order.create(products, registeredDateTime);
+		Order order = Order.create(duplicateProducts, registeredDateTime);
 		Order savedOrder = orderRepository.save(order);
 
 		return OrderResponse.of(savedOrder);
