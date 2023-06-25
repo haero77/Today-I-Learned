0ff837c6d0fa9975169e7290155e2b884d9c1f40
Seonho Kim <sunhowold@naver.com>
2023-06-25 20:46:11 +0900

    [inf-stock] Pessimistic Lock 구현

--- _02_Spring/projects/inf_spring_stock/src/main/java/com/stock/repository/StockRepository.java
+++ _02_Spring/projects/inf_spring_stock/src/main/java/com/stock/repository/StockRepository.java
@@ -1,8 +1,17 @@
 package com.stock.repository;
 
+import javax.persistence.LockModeType;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.stock.domain.Stock;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
+
+	@Lock(value = LockModeType.PESSIMISTIC_WRITE)
+	@Query("select s from Stock s where s.id = :id")
+	Stock findByIdWithPessimisticLock(@Param(value = "id") Long id);
 }
