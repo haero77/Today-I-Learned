0dae0e29c7677e3603e375ab8e991f2be7134cfe
Seonho Kim <sunhowold@naver.com>
2023-06-26 16:56:15 +0900

    [inf-testcode] Presentation Layer (2): OrderController 성공, 실패 TC

--- _05_Test_Code/projects/practical_test/src/main/java/sample/cafekiosk/spring/api/controller/order/request/OrderCreateRequest.java
+++ _05_Test_Code/projects/practical_test/src/main/java/sample/cafekiosk/spring/api/controller/order/request/OrderCreateRequest.java
@@ -2,6 +2,8 @@ package sample.cafekiosk.spring.api.controller.order.request;
 
 import java.util.List;
 
+import javax.validation.constraints.NotEmpty;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,6 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class OrderCreateRequest {
 
+	@NotEmpty(message = "상품 번호 리스트는 필수입니다.")
 	private List<String> productNumbers;
 
 	@Builder // 나중에 어떤 필드가 추가될지 모르므로 빌더를 열어둔다.
