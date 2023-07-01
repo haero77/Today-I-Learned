667eb3bfa0436b612c21e717eead00a93b6bd6d3
Seonho Kim <sunhowold@naver.com>
2023-07-01 23:47:43 +0900

    [inf-spring-data-jpa] 예제 도메인 모델

--- _03_JPA/projects/_03_data_jpa/src/main/java/inf/datajpa/entity/Member.java
+++ _03_JPA/projects/_03_data_jpa/src/main/java/inf/datajpa/entity/Member.java
@@ -1,29 +1,61 @@
 package inf.datajpa.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.xml.stream.events.StartDocument;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 @Entity
 public class Member {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "member_id")
 	private Long id;
 
 	private String username;
 
-	protected Member() {
-	}
+	private int age;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "team_id")
+	private Team team;
 
 	public Member(String username) {
+		this(username, 0);
+	}
+
+	public Member(String username, int age) {
+		this(username, age, null);
+	}
+
+	public Member(String username, int age, Team team) {
 		this.username = username;
+		this.age = age;
+		if (team != null) {
+			this.team = team;
+		}
+	}
+
+	public void changeTeam(Team team) {
+		this.team = team;
+		team.getMembers().add(this);
 	}
 
 }
