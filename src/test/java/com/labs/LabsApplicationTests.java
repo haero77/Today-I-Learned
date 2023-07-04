57461af0e1ea0986d4fbe67b274b7074ead99670
Seonho Kim <sunhowold@naver.com>
2023-07-04 21:56:55 +0900

    labs application initial commit

---
+++ src/test/java/com/labs/LabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
