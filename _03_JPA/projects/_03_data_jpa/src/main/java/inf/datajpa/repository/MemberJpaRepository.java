d4d2a7e473e5d6f507073fb90e424706cc373d55
Seonho Kim <sunhowold@naver.com>
2023-07-10 18:08:00 +0900

    [inf-spring-data-jpa] 쿼리 메소드 기능 - 순수 JPA 페이징과 정렬

--- _03_JPA/projects/_03_data_jpa/src/main/java/inf/datajpa/repository/MemberJpaRepository.java
+++ _03_JPA/projects/_03_data_jpa/src/main/java/inf/datajpa/repository/MemberJpaRepository.java
@@ -44,4 +44,27 @@ public class MemberJpaRepository {
 		return em.find(Member.class, id);
 	}
 
+	/**
+	 * - 아래 조건을 페이징 하고 싶다고 한다.
+	 *   - 검색 조건: 나이가 10살
+	 *   - 정렬 조건: 이름으로 내림차순
+	 *   - 페이징 조건: 첫 번째 페이지, 페이지당 보여줄 데이터는 3건
+	 */
+	public List<Member> findByPage(int age, int offset, int limit) {
+		return em.createQuery(
+			"select m from Member m where m.age = :age order by m.username desc",
+				Member.class
+			)
+			.setParameter("age", age)
+			.setFirstResult(offset)
+			.setMaxResults(limit)
+			.getResultList();
+	}
+
+	public long totalCount(int age) {
+		return em.createQuery("select count(m) from Member m where m.age = :age", Long.class)
+			.setParameter("age", age)
+			.getSingleResult();
+	}
+
 }
