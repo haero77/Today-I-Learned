07f307e54a5980f23fdd46a31e5ffcefdcb03d2e
Seonho Kim <sunhowold@naver.com>
2023-07-11 00:57:26 +0900

    [inf-spring-data-jpa] 쿼리 메소드 기능 - 스프링 데이터 JPA 페이징과 정렬

--- _03_JPA/projects/_03_data_jpa/src/main/java/inf/datajpa/repository/MemberRepository.java
+++ _03_JPA/projects/_03_data_jpa/src/main/java/inf/datajpa/repository/MemberRepository.java
@@ -4,6 +4,9 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -32,5 +35,23 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
 	@Query("select m from Member m where m.username in :names")
 	List<Member> findByNames(@Param("names") Collection<String> names);
+
+	Page<Member> findAsPageByAge(int age, Pageable pageable);
+
+	Slice<Member> findAsSliceByAge(int age, Pageable pageable);
+
+	List<Member> findAsListByAge(int age, Pageable pageable);
+
+	// 카운트 쿼리와 페치 조인 못 쓴다.
+	// @Query(value = "select m from Member m join fetch m.team t")
+	// Page<Member> findWithFetchJoinWithoutCountQueryByAge(int age, Pageable pageable);
+
+	// Page 리턴 타입 때문에 COUNT 쿼리가 추가로 실행 될 때, COUNT 쿼리에서도 JOIN이 실행되는 문제
+	@Query(value = "select m from Member m left join m.team t")
+	Page<Member> findWithoutCountQueryByAge(int age, Pageable pageable);
+
+	@Query(value = "select m from Member m left join m.team t",
+		countQuery = "select count(m) from Member m")
+	Page<Member> findWithCountQueryByAge(int age, Pageable pageable);
 
 }
