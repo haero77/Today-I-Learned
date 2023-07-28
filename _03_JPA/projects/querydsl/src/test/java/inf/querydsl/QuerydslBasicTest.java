9c115341a71d77bc3506be33249b1160d22a9c8a
haero77 <shineecard5@gmail.com>
2023-07-28 21:58:20 +0900

    [inf-querydsl] 3.6 페이징

--- _03_JPA/projects/querydsl/src/test/java/inf/querydsl/QuerydslBasicTest.java
+++ _03_JPA/projects/querydsl/src/test/java/inf/querydsl/QuerydslBasicTest.java
@@ -152,4 +152,19 @@ public class QuerydslBasicTest {
         assertThat(memberNull.getUsername()).isNull();
     }
 
+    @Test
+    void paging2() {
+        QueryResults<Member> queryResults = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetchResults();
+
+        assertThat(queryResults.getTotal()).isEqualTo(4);
+        assertThat(queryResults.getLimit()).isEqualTo(2);
+        assertThat(queryResults.getOffset()).isEqualTo(1);
+        assertThat(queryResults.getResults().size()).isEqualTo(2);
+    }
+
 }
