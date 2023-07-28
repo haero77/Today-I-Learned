9ee9f8a07d270e0ba7e3fa9fe3c50afab10e7f52
haero77 <shineecard5@gmail.com>
2023-07-28 21:45:04 +0900

    [inf-querydsl] 3.5 정렬

--- _03_JPA/projects/querydsl/src/test/java/inf/querydsl/QuerydslBasicTest.java
+++ _03_JPA/projects/querydsl/src/test/java/inf/querydsl/QuerydslBasicTest.java
@@ -122,4 +122,34 @@ public class QuerydslBasicTest {
                 .fetchCount();
     }
 
+    /**
+     * 회원 정렬 순서
+     * 1. 회원 나이 내림차순 (desc)
+     * 2. 회원 이름 올림차순 (asc)
+     * 단 2에서 회원 이름이 없으면 마지에 출력(nulls last)
+     */
+    @Test
+    void sort() {
+        // given
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        // when
+        List<Member> findMembers = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast()) // or nullsFirst
+                .fetch();
+
+        // then
+        Member member5 = findMembers.get(0);
+        Member member6 = findMembers.get(1);
+        Member memberNull = findMembers.get(2);
+
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
+    }
+
 }
