0feda262357e7e9e7017bbe9539c4829c6e4b10e
haero77 <shineecard5@gmail.com>
2023-08-12 22:32:04 +0900

    [inf-querydsl] (예제) 5.4 조회 컨트롤러 API 구현

--- _03_JPA/projects/querydsl/src/main/java/inf/querydsl/dto/MemberTeamDto.java
+++ _03_JPA/projects/querydsl/src/main/java/inf/querydsl/dto/MemberTeamDto.java
@@ -1,16 +1,14 @@
 package inf.querydsl.dto;
 
 import com.querydsl.core.annotations.QueryProjection;
-import lombok.Data;
 
-@Data
 public class MemberTeamDto {
 
-	private Long memberId;
-	private String username;
-	private int age;
-	private Long teamId;
-	private String teamName;
+	private final Long memberId;
+	private final String username;
+	private final int age;
+	private final Long teamId;
+	private final String teamName;
 
 	@QueryProjection
 	public MemberTeamDto(Long memberId, String username, int age, Long teamId, String teamName) {
--- _03_JPA/projects/querydsl/src/main/java/inf/querydsl/entity/Team.java
+++ _03_JPA/projects/querydsl/src/main/java/inf/querydsl/entity/Team.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class Team {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "team_id")
     private Long id;
 
