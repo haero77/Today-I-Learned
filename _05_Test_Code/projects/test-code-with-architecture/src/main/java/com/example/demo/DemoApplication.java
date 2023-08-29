19ecc064849beaefe6ed79ac6e8a6e8ef969d530
haero77 <shineecard5@gmail.com>
2023-08-30 00:49:57 +0900

    [Test-오답노트] 2.2 테스트 추가하기: h2를 이용한 repository 테스트

--- _05_Test_Code/projects/test-code-with-architecture/src/main/java/com/example/demo/DemoApplication.java
+++ _05_Test_Code/projects/test-code-with-architecture/src/main/java/com/example/demo/DemoApplication.java
@@ -7,9 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(
-    title = "Toy project",
-    version = "v1",
-    description = "테스트 코드를 추가하기 위한 연습에 사용될 토이 프로젝트입니다"
+        title = "Toy project",
+        version = "v1",
+        description = "테스트 코드를 추가하기 위한 연습에 사용될 토이 프로젝트입니다"
 ))
 public class DemoApplication {
 
--- _05_Test_Code/projects/test-code-with-architecture/src/main/java/com/example/demo/repository/UserRepository.java
+++ _05_Test_Code/projects/test-code-with-architecture/src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,12 +1,14 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.UserStatus;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     Optional<UserEntity> findByIdAndStatus(long id, UserStatus userStatus);
 
     Optional<UserEntity> findByEmailAndStatus(String email, UserStatus userStatus);
+
 }
