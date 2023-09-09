e5130d2f88fd6937bfde17a72946dd2e7d61a1ec
haero77 <shineecard5@gmail.com>
2023-09-09 10:57:15 +0900

    [Test-오답노트] 4.2 외부 연동 다루기 - CertificationService 테스트 추가

---
+++ _05_Test_Code/projects/test-code-with-architecture/src/test/java/com/example/demo/user/service/CertificationServiceTest.java
@@ -0,0 +1,27 @@
+package com.example.demo.user.service;
+
+import com.example.demo.mock.FakeMailSender;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CertificationServiceTest {
+
+    @DisplayName("이메일과 컨텐츠가 제대로 만들어져서 보내지는지 테스트한다.")
+    @Test
+    void send() {
+        // given
+        FakeMailSender fakeMailSender = new FakeMailSender();
+        CertificationService certificationService = new CertificationService(fakeMailSender);
+
+        // when
+        certificationService.send("kok202@naver.com", 1, "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab");
+
+        // then
+        assertThat(fakeMailSender.email).isEqualTo("kok202@naver.com");
+        assertThat(fakeMailSender.title).isEqualTo("Please certify your email address");
+        assertThat(fakeMailSender.content).isEqualTo("Please click the following link to certify your email address: http://localhost:8080/api/users/1/verify?certificationCode=aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab");
+    }
+
+}
