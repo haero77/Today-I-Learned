d65f41dfb27cfa5e4de5c7ed3947dcd628d749d1
haero77 <shineecard5@gmail.com>
2023-09-29 16:17:40 +0900

    [Test-오답노트] 4.6 컨트롤러를 소형 테스트로 - CertificationService 를 추상화 하지 않기
    CertificationService 는 컨트롤러같은 외부 호출이 없으니 굳이 추상화 안 해도 될 것 같다.

--- _05_Test_Code/projects/test-code-with-architecture/src/main/java/com/example/demo/user/controller/port/CertificationService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.user.controller.port;
-
-// FIXME: CertificationService 를 컨트롤러에 두면 하위 모듈인 서비스가 상위 모듈인 컨트롤러를 의존
-public interface CertificationService {
-
-    void send(String email, long userId, String certificationCode);
-
-}
