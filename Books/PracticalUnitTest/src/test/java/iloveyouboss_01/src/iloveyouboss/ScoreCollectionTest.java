6490f81cb59b888c96137dd52fb76b9073876052
haero77 <shineecard5@gmail.com>
2023-10-26 23:35:35 +0900

    (self) 	결함이나 한계점을 드러내는 테스트 작성

--- Books/PracticalUnitTest/src/test/java/iloveyouboss_01/src/iloveyouboss/ScoreCollectionTest.java
+++ Books/PracticalUnitTest/src/test/java/iloveyouboss_01/src/iloveyouboss/ScoreCollectionTest.java
@@ -1,7 +1,9 @@
 package iloveyouboss_01.src.iloveyouboss;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -20,6 +22,19 @@ class ScoreCollectionTest {
 
         // then
         assertThat(actualResult).isEqualTo(6);
+    }
+
+    // 셀프 테스트 추가
+    @DisplayName("아무런 숫자가 주어지지 않았을 때 산술 평균을 구할 시 예외가 발생한다.")
+    @Test
+    void throws_exception_when_no_number_given() {
+        // given
+        ScoreCollection collection = new ScoreCollection();
+
+        // when & then
+        assertThatThrownBy(() -> collection.arithmeticMean())
+                .isInstanceOf(ArithmeticException.class)
+                .hasMessageContaining("/ by zero");
     }
 
 }
