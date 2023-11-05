2f82b336e613ceb5833a1c6d5ead26c90122bba6
haero77 <shineecard5@gmail.com>
2023-11-05 20:41:04 +0900

    ch02) 테스트03: 숫자 포함 조건 확인 로직을 메서드로 분리 (리팩토링)

--- _05_Test_Code/projects/TDD_Start/src/test/java/chap02/PasswordStrengthMeter.java
+++ _05_Test_Code/projects/TDD_Start/src/test/java/chap02/PasswordStrengthMeter.java
@@ -7,19 +7,22 @@ public class PasswordStrengthMeter {
             return PasswordStrength.NORMAL;
         }
 
-        boolean containsNum = false;
-        for (char ch : s.toCharArray()) {
-            if (ch >= '0' && ch <= '9') {
-                containsNum = true;
-                break;
-            }
-        }
+        boolean containsNum = meetsContainingNumberCriteria(s);
 
         if (!containsNum) {
             return PasswordStrength.NORMAL;
         }
 
         return PasswordStrength.STRONG;
+    }
+
+    private static boolean meetsContainingNumberCriteria(String s) {
+        for (char ch : s.toCharArray()) {
+            if (ch >= '0' && ch <= '9') {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
