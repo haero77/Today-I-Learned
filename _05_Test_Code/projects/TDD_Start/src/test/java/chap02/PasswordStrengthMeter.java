32b2a2bec4b93f494dce289d49a85dfdfe5f0838
haero77 <shineecard5@gmail.com>
2023-11-05 20:32:18 +0900

    ch02) 테스트02: 새로 추가한 테스트 코드를 성공 시킬 정도로만 코드를 수정한다.

--- _05_Test_Code/projects/TDD_Start/src/test/java/chap02/PasswordStrengthMeter.java
+++ _05_Test_Code/projects/TDD_Start/src/test/java/chap02/PasswordStrengthMeter.java
@@ -3,6 +3,9 @@ package chap02;
 public class PasswordStrengthMeter {
 
     public PasswordStrength meter(String s) {
+        if (s.length() < 8) {
+            return PasswordStrength.NORMAL;
+        }
         return PasswordStrength.STRONG;
     }
 
