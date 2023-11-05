9697dd19b2b95e2f0acb959266a5a9f79c59483f
haero77 <shineecard5@gmail.com>
2023-11-05 20:27:15 +0900

    ch02) 테스트01: 모든 규칙을 충족하는 예 추가

--- _05_Test_Code/projects/TDD_Start/src/test/java/chap02/PasswordStrengthMeterTest.java
+++ _05_Test_Code/projects/TDD_Start/src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -11,8 +11,11 @@ public class PasswordStrengthMeterTest {
     @Test
     void meetsAllCriteria_Then_Strong() {
         PasswordStrengthMeter meter = new PasswordStrengthMeter();
-        PasswordStrength strength = meter.meter("ab12!@AB");
-        assertThat(strength).isEqualTo(PasswordStrength.STRONG);
+        PasswordStrength result = meter.meter("ab12!@AB");
+        assertThat(result).isEqualTo(PasswordStrength.STRONG);
+
+        PasswordStrength result2 = meter.meter("abc1!Add");
+        assertThat(result2).isEqualTo(PasswordStrength.STRONG);
     }
 
 }
