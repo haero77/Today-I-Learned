6a5d2ef9bbf64d973914f5269e9d3d95a7b92400
haero77 <shineecard5@gmail.com>
2023-11-08 14:04:35 +0900

    ch02) 코드 정리: 테스트 코드 정리(무턱 대고 테스트 코드의 중복을 제거하면 안 된다!)
    - 테스트 코드 중복 무턱대고 제거하면 안 됨.
    - 중복 제거 후 테스트 코드 가독성이 떨어지지 않고 수정이 용이한 경우에만.
    - 중복 제거 후 오히려 관리가 어려워지만 중복을 되돌려야함

--- _05_Test_Code/projects/TDD_Start/src/test/java/chap02/PasswordStrengthMeterTest.java
+++ _05_Test_Code/projects/TDD_Start/src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -7,31 +7,29 @@ import org.junit.jupiter.api.Test;
 
 public class PasswordStrengthMeterTest {
 
+    private final PasswordStrengthMeter meter = new PasswordStrengthMeter();
+
+    private void assertStrength(String password, PasswordStrength expectedStrength) {
+        assertThat(meter.meter(password)).isEqualTo(expectedStrength);
+    }
+
     @DisplayName("암호가 모든 조건을 충족하면 암호강도는 강함이어야 함")
     @Test
     void meetsAllCriteria_Then_Strong() {
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
-        PasswordStrength result = meter.meter("ab12!@AB");
-        assertThat(result).isEqualTo(PasswordStrength.STRONG);
-
-        PasswordStrength result2 = meter.meter("abc1!Add");
-        assertThat(result2).isEqualTo(PasswordStrength.STRONG);
+        assertStrength("ab12!@AB", PasswordStrength.STRONG);
+        assertStrength("abc1!Add", PasswordStrength.STRONG);
     }
 
     @DisplayName("길이가 8글자 미만이고 나머지 조건은 충족하는 경우 암호강도는 보통이다.")
     @Test
     void meetOtherCriteria_except_for_Length_Then_Normal() {
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
-        PasswordStrength result = meter.meter("ab12!@A");
-        assertThat(result).isEqualTo(PasswordStrength.NORMAL);
+        assertStrength("ab12!@A", PasswordStrength.NORMAL);
     }
 
     @DisplayName("숫자를 포함하지 않고 나머지 조건을 충족할 경우 암호 강도는 보통이다.")
     @Test
     void meetOtherCriteria_except_for_number_Then_Normal() {
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
-        PasswordStrength result = meter.meter("ab!@ABqwer");
-        assertThat(result).isEqualTo(PasswordStrength.NORMAL);
+        assertStrength("ab!@ABqwer", PasswordStrength.NORMAL);
     }
 
 }
