403224e0794ea5c3ee9cd7ba7651c1585cc65ca5
haero77 <shineecard5@gmail.com>
2023-11-25 11:35:23 +0900

    AOPUtils.isAopProxy 를 이용한 aop 프록시 적용 확인하기

---
+++ _02_Spring/projects/advanced_aop/src/test/java/hello/aop/AopTest.java
@@ -0,0 +1,28 @@
+package hello.aop;
+
+import hello.aop.order.OrderRepository;
+import hello.aop.order.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+public class AopTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    void aopInfo() {
+        // orderService 가 aop proxy 가 적용되었는지 확인
+        log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
+    }
+
+}
