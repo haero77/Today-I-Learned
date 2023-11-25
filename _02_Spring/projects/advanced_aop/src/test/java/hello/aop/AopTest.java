c99a588829d479ed7e3665a1158627f6ccbbc734
haero77 <shineecard5@gmail.com>
2023-11-25 14:29:15 +0900

    10.3 @Import를 사용하여 aspect를 빈 등록

--- _02_Spring/projects/advanced_aop/src/test/java/hello/aop/AopTest.java
+++ _02_Spring/projects/advanced_aop/src/test/java/hello/aop/AopTest.java
@@ -4,13 +4,16 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV1;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @Slf4j
+@Import(AspectV1.class)
 @SpringBootTest
 public class AopTest {
 
