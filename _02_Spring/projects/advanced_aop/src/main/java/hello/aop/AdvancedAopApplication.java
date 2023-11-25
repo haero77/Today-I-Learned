ad3df2f59d6971a4b8fa89d578dd738e590214bc
haero77 <shineecard5@gmail.com>
2023-11-25 11:26:41 +0900

    예제 service, repository 추가

--- _02_Spring/projects/advanced_aop/src/main/java/hello/aop/AdvancedAopApplication.java
+++ _02_Spring/projects/advanced_aop/src/main/java/hello/aop/AdvancedAopApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AdvancedAopApplication {
 
 	public static void main(String[] args) {
+		// 웹(톰캣)이 없으므로 실행되고 바로 종료.
 		SpringApplication.run(AdvancedAopApplication.class, args);
 	}
 
