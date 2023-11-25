e001cc63faeda34511cb7dd2b187b3f43fcbe26c
haero77 <shineecard5@gmail.com>
2023-11-25 15:12:03 +0900

    10.6 포인트컷 참조

---
+++ _02_Spring/projects/advanced_aop/src/main/java/hello/aop/order/aop/Pointcuts.java
@@ -0,0 +1,25 @@
+package hello.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Slf4j
+@Aspect
+public class Pointcuts {
+
+    // 타겟은 hello.aop.order 패키지와 그 하위 패키지
+    @Pointcut("execution(* hello.aop.order..*(..))") // pointcut experssion
+    public void allOrder() { // pointcut signature. 여기서의 포인트컷 시그니처는 'allOrder()'
+    }
+
+    // 타입 이름(인터페이스, 클래스) 패턴이 *Service
+    @Pointcut("execution(* *..*Service.*(..))")
+    public void allService() {
+    }
+
+    @Pointcut("allOrder() && allService()")
+    public void allOrderAndService() {
+    }
+
+}
--- _02_Spring/projects/advanced_aop/src/test/java/hello/aop/AopTest.java
+++ _02_Spring/projects/advanced_aop/src/test/java/hello/aop/AopTest.java
@@ -6,6 +6,7 @@ import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
 import hello.aop.order.aop.AspectV2;
 import hello.aop.order.aop.AspectV3;
+import hello.aop.order.aop.AspectV4Pointcut;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
@@ -16,7 +17,8 @@ import org.springframework.context.annotation.Import;
 @Slf4j
 //@Import(AspectV1.class)
 //@Import(AspectV2.class)
-@Import(AspectV3.class)
+//@Import(AspectV3.class)
+@Import(AspectV4Pointcut.class)
 @SpringBootTest
 public class AopTest {
 
