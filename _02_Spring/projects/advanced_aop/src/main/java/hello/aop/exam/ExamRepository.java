f0b0c3ec2a98a9975e009ebff0ee1505e12252c4
haero77 <shineecard5@gmail.com>
2023-11-28 21:08:47 +0900

    12. 실전 예제 - 재시도 AOP

--- _02_Spring/projects/advanced_aop/src/main/java/hello/aop/exam/ExamRepository.java
+++ _02_Spring/projects/advanced_aop/src/main/java/hello/aop/exam/ExamRepository.java
@@ -1,8 +1,11 @@
 package hello.aop.exam;
 
+import hello.aop.exam.annotation.Retry;
 import hello.aop.exam.annotation.Trace;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
+@Slf4j
 @Repository
 public class ExamRepository {
 
@@ -13,8 +16,10 @@ public class ExamRepository {
      * 이렇게 간헐적으로 실패할 경우 재시도하는 AOP가 있으면 편리하다.
      */
     @Trace
+    @Retry(value = 4)
     public String save(String itemId) {
         seq++;
+        log.info("seq={}", seq);
 
         if (seq % 5 == 0) {
             throw new IllegalArgumentException("예외 발생");
