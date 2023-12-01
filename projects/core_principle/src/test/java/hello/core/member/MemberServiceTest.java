a1986d99d913cec88d4738f2ffd575f6e46d6ca0
haero77 <shineecard5@gmail.com>
2023-12-01 13:50:25 +0900

    회원 서비스가 OCP, DIP를 지키지 못하는 이유: 구현체에 의존

--- projects/core_principle/src/test/java/hello/core/member/MemberServiceTest.java
+++ projects/core_principle/src/test/java/hello/core/member/MemberServiceTest.java
@@ -8,15 +8,6 @@ class MemberServiceTest {
 
     MemberService memberService = new MemberServiceImpl();
 
-    /**
-     * 회원 도메인 설계의 문제점
-     * 이 코드의 설계상 문제점은 무엇일까요?
-     * 다른 저장소로 변경할 때 OCP 원칙을 잘 준수할까요?
-     * DIP를 잘 지키고 있을까요?
-     * 의존관계가 인터페이스 뿐만 아니라 구현까지 모두 의존하는 문제점이 있음
-     * 주문까지 만들고나서 문제점과 해결 방안을 설명
-     */
-
     @Test
     void join() {
         // given
