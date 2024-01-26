b213380402c93f98b18ef7f4edde7f37e9fea22b
haero77 <shineecard5@gmail.com>
2024-01-27 03:27:27 +0900

    [실용 테스트] initial commit

---
+++ TestCode/박우빈 - Practical Testing: 실용적인 테스트 가이드/testcode-practical/src/main/java/cafekiosk/CafeKioskApplication.java
@@ -0,0 +1,13 @@
+package cafekiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeKioskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeKioskApplication.class, args);
+	}
+
+}
