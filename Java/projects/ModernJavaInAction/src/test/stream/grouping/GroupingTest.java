8a37a3da788eb06a34f04131891834c6b1df1d69
haero77 <shineecard5@gmail.com>
2024-01-29 00:40:28 +0900

    [모자인] stream grouping - flatMapping

--- Java/projects/ModernJavaInAction/src/test/stream/grouping/GroupingTest.java
+++ Java/projects/ModernJavaInAction/src/test/stream/grouping/GroupingTest.java
@@ -10,18 +10,21 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.*;
 
 public class GroupingTest {
 
-	List<Dish> dishes = Menu.menu;
+	List<Dish> menu = Menu.menu;
 
 	@DisplayName("grouping by field - type")
 	@Test
 	void asd() {
-		Map<DishType, List<Dish>> dishesByType = this.dishes.stream()
+		Map<DishType, List<Dish>> dishesByType = this.menu.stream()
 				.collect(groupingBy(dish -> dish.getType()));
 
 		System.out.println("something");
@@ -30,7 +33,7 @@ public class GroupingTest {
 	@DisplayName("그룹화 2")
 	@Test
 	void grouping2() {
-		Map<CaloricLevel, List<Dish>> dishesByCaloricLevel = this.dishes.stream()
+		Map<CaloricLevel, List<Dish>> dishesByCaloricLevel = this.menu.stream()
 				.collect(groupingBy(dish -> {
 					if (dish.getCalories() < 400) {
 						return CaloricLevel.DIET;
@@ -42,33 +45,52 @@ public class GroupingTest {
 	@DisplayName("500 칼로리가 넘는 요소 필터링")
 	@Test
 	void filter() {
-		// 문제: 500 칼로리를 넘기는 FISH 가 없으므로, 결과 맵에서 해당 키(FISH) 자체가 사라진다.ff
-		Map<DishType, List<Dish>> caloricDishesByType = dishes.stream()
+		// 문제: 500 칼로리를 넘기는 FISH 가 없으므로, 결과 맵에서 해당 키(FISH) 자체가 사라진다.
+		Map<DishType, List<Dish>> caloricDishesByType = menu.stream()
 				.filter(dish -> dish.getCalories() > 500)
 				.collect(groupingBy(Dish::getType));
 
 		System.out.println(caloricDishesByType);
 
 		// groupingBy의 팩토리 메서드를 오버로드해 문제를 해결
-		Map<DishType, List<Dish>> dishes2 = dishes.stream()
+		Map<DishType, List<Dish>> dishes2 = menu.stream()
 				.collect(groupingBy(Dish::getType, filtering(dish -> dish.getCalories() > 500, toList())));
 
 		System.out.println(dishes2);
 
 		// 맵핑
-		Map<DishType, List<String>> dishNamesByType = dishes.stream()
+		Map<DishType, List<String>> dishNamesByType = menu.stream()
 				.collect(groupingBy(Dish::getType, mapping(Dish::getName, toList())));
 
 		System.out.println(dishNamesByType);
 	}
-	
-	
+
+
 	@DisplayName("flatMapping")
 	@Test
 	void flatMapping() {
 		Map<String, List<String>> dishTags = new HashMap<>();
 		dishTags.put("pork", asList("greasy", "salty"));
-		dishTags.put("beef", asList("fried", "crisp"));
+		dishTags.put("beef", asList("salty", "roasted"));
+		dishTags.put("chicken", asList("fried", "crisp"));
+		dishTags.put("french fries", asList("greasy", "fried"));
+		dishTags.put("rice", asList("light", "natural"));
+		dishTags.put("season fruit", asList("fresh", "natural"));
+		dishTags.put("pizza", asList("tasty", "salty"));
+		dishTags.put("prawns", asList("tasty", "roasted"));
+		dishTags.put("salmon", asList("delicious", "fresh"));
+
+		Map<DishType, Set<String>> dishNamesByType = menu.stream().collect(groupingBy(
+				Dish::getType,
+				Collectors.flatMapping(dish -> dishTags.get(dish.getName()).stream(), toSet())
+		));
+
+		Map<DishType, Set<Stream<String>>> collect = menu.stream().collect(groupingBy(
+				Dish::getType,
+				mapping(dish -> dishTags.get(dish.getName()).stream(), toSet())
+		));
+
+		System.out.println(dishNamesByType);
 	}
 
 }
