0ea1f6853474d2063e977e03d368dd44ae931f32
haero77 <shineecard5@gmail.com>
2024-02-20 10:33:56 +0900

    [TDD Start] (source) 구독 만료일 계산기 - 코드 정리: 중복 제거 - 미리 단정지어 수정 X, 테스트 코드 중복 제거는 고민 필요(테스트 메서드 스스로 명확한 설명이 필요)

--- TestCode/projects/tdd-start-madvirus/src/test/java/com/example/tddstartmadvirus/chap03/ExpiryDateCalculatorTest.java
+++ TestCode/projects/tdd-start-madvirus/src/test/java/com/example/tddstartmadvirus/chap03/ExpiryDateCalculatorTest.java
@@ -12,22 +12,30 @@ public class ExpiryDateCalculatorTest {
 	@DisplayName("1만원 납부할 시, 만료일은 한달 뒤이다.")
 	@Test
 	void ten_thousand_won() {
-		LocalDate billingDate = LocalDate.of(2019, 3, 1);
-		int payAmount = 10_000;
+		assertExpiryDate(
+				LocalDate.of(2019, 3, 1), 10_000,
+				LocalDate.of(2019, 4, 1));
+		assertExpiryDate(
+				LocalDate.of(2019, 5, 5), 10_000,
+				LocalDate.of(2019, 6, 5));
+	}
 
+	/**
+	 * 보통은 중복을 제거하는 것이 좋지만, 테스트 코드의 중복 제거는 고민 필요.
+	 * 👉 각 테스트 메서드는 스스로 무엇을 테스트하는지 명확하게 설명할 수 있어야 하기 때문.
+	 */
+	/**
+	 * 첫 번째 파라미터와 세 번째 파라미터가 둘 다 LocalDate이므로,
+	 * 둘 중 어떤 파라미터가 납부일이고 어떤 파라미터가 기댓값인지 구분하려면 이 메서드의 구현을 살펴봐야한다.
+	 *
+	 * 그래도 해당 메서드가 길지 않고,
+	 * 파라미터 개수도 세 개여서 테스트 코드를 볼 때 어떤 것을 검증하는지 쉽게 확인가능하다.
+	 * 👉 이 정도면 중복을 제거 해도 괜찮겠다고 판단.
+	 */
+	private void assertExpiryDate(LocalDate billingDate, int payAmount, LocalDate expectedExpiryDate) {
 		ExpiryDateCalculator cal = new ExpiryDateCalculator();
 		LocalDate expiryDate = cal.calculateExpiryDate(billingDate, payAmount);
-
-		assertThat(expiryDate).isEqualTo(LocalDate.of(2019, 4, 1));
-
-
-		LocalDate billingDate2 = LocalDate.of(2019, 5, 5);
-		int payAmount2 = 10_000;
-
-		ExpiryDateCalculator cal2 = new ExpiryDateCalculator();
-		LocalDate expiryDate2 = cal2.calculateExpiryDate(billingDate2, payAmount2);
-
-		assertThat(expiryDate2).isEqualTo(LocalDate.of(2019, 6, 5));
+		assertThat(expiryDate).isEqualTo(expectedExpiryDate);
 	}
 
 }
