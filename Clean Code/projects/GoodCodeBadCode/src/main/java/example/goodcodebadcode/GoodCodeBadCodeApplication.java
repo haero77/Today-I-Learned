f3febe35740f56c0303dbdbca051e4a5c3e8913a
haero77 <shineecard5@gmail.com>
2024-02-27 23:27:25 +0900

    [좋은 코드, 나쁜 코드] source update

---
+++ Clean Code/projects/GoodCodeBadCode/src/main/java/example/goodcodebadcode/GoodCodeBadCodeApplication.java
@@ -0,0 +1,13 @@
+package example.goodcodebadcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodCodeBadCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodCodeBadCodeApplication.class, args);
+	}
+
+}
---
+++ Clean Code/projects/GoodCodeBadCode/src/test/java/example/goodcodebadcode/GoodCodeBadCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package example.goodcodebadcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodCodeBadCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
