f72e59cc55d7073e1a48e91df77fefa0e8eb1224
haero77 <shineecard5@gmail.com>
2024-04-23 21:42:44 +0900

    [TDD] 만료일 계산기 tdd 연습 - 리팩토링 과정 1: 기존 프로덕션이 새로 만든 프로덕션을 호출하도록

--- TestCode/projects/tdd-start-madvirus/src/test/java/com/example/tddstartmadvirus/chap03/ExpiryDateCalculator.java
+++ TestCode/projects/tdd-start-madvirus/src/test/java/com/example/tddstartmadvirus/chap03/ExpiryDateCalculator.java
@@ -1,10 +1,22 @@
 package com.example.tddstartmadvirus.chap03;
 
+import net.bytebuddy.asm.Advice;
+
 import java.time.LocalDate;
 
 public class ExpiryDateCalculator {
 
     public LocalDate calculate(LocalDate billingDate, int payAmount) {
+        PayData payData = PayData.builder()
+                .billingDate(billingDate)
+                .payAmount(payAmount)
+                .build();
+
+        return calculate(payData);
+    }
+
+    public LocalDate calculate(PayData payData) {
+        LocalDate billingDate = payData.getBillingDate();
         return billingDate.plusMonths(1L);
     }
 
