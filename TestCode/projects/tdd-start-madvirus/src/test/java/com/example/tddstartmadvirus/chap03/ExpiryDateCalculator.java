e3523b7e123386cb91a2310cd1bab75d7826cab5
haero77 <shineecard5@gmail.com>
2024-04-23 21:06:23 +0900

    Revert "[TDD] 만료일 계산기 tdd 연습 - 리팩토링 과정 1"
    This reverts commit 4ece7a7c7a039ce6ca5a0e461d7da161880bbf35.

--- TestCode/projects/tdd-start-madvirus/src/test/java/com/example/tddstartmadvirus/chap03/ExpiryDateCalculator.java
+++ TestCode/projects/tdd-start-madvirus/src/test/java/com/example/tddstartmadvirus/chap03/ExpiryDateCalculator.java
@@ -5,15 +5,7 @@ import java.time.LocalDate;
 public class ExpiryDateCalculator {
 
     public LocalDate calculate(LocalDate billingDate, int payAmount) {
-        PayData payData = PayData.builder()
-                .billingDate(billingDate)
-                .payAmount(payAmount)
-                .build();
-        return calculate(payData);
-    }
-
-    public LocalDate calculate(PayData payData) {
-        return payData.getBillingDate().plusMonths(1L);
+        return billingDate.plusMonths(1L);
     }
 
 }
