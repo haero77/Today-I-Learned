b70c0b8d3983333acb04f8f36f1caf72d8c829c6
haero77 <shineecard5@gmail.com>
2024-07-11 18:19:48 +0900

    [multi-module] (gemini) build.gradle 로 db 모듈을 먼저 빌드해도 bean을 찾지 못하는 버그 👉 수동으로 db 모듈을 먼저 빌드하면 실행된다.

---
+++ Java/projects/multi-module-gemini/core/core-api/src/main/java/til/example/multimodulelabs/CoreApiApplication.java
@@ -0,0 +1,12 @@
+package til.example.multimodulelabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreApiApplication.class, args);
+    }
+}
