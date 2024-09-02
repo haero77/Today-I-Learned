01e402e1744feae6dc46f9748502b433510e84f9
haero77 <shineecard5@gmail.com>
2024-09-03 00:56:35 +0900

    [security] 완전 정복 예제 init

---
+++ Spring/projects/jungsoowon/security-conquer/src/main/java/com/example/securityconquer/SecurityConquerApplication.java
@@ -0,0 +1,12 @@
+package com.example.securityconquer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityConquerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityConquerApplication.class, args);
+    }
+}
---
+++ Spring/projects/jungsoowon/security-conquer/src/test/java/com/example/securityconquer/SecurityConquerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityconquer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityConquerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
