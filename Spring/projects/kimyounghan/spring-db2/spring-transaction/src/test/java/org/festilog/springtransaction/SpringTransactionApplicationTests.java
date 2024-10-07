86524b11431e04390cbbfe6cfab285bb2ec8a361
haero77 <shineecard5@gmail.com>
2024-10-07 18:02:03 +0900

    [spring-db-2] 예제 initial commit

---
+++ Spring/projects/kimyounghan/spring-db2/spring-transaction/src/test/java/org/festilog/springtransaction/SpringTransactionApplicationTests.java
@@ -0,0 +1,12 @@
+package org.festilog.springtransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
