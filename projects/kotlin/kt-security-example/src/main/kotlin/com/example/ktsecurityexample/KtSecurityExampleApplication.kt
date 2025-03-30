03cc1bd05c73464f6632e84c805965543ed11648
haero77 <shineecard5@gmail.com>
2025-03-30 20:54:09 +0900

    [kotlin] 코틀린 시큐리티 예제 init

---
+++ projects/kotlin/kt-security-example/src/main/kotlin/com/example/ktsecurityexample/KtSecurityExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ktsecurityexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KtSecurityExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<KtSecurityExampleApplication>(*args)
+}
---
+++ projects/kotlin/kt-security-example/src/test/kotlin/com/example/ktsecurityexample/KtSecurityExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ktsecurityexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KtSecurityExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
