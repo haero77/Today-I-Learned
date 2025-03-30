19a61e8073b2496b8c86727860da955b8520abb7
haero77 <shineecard5@gmail.com>
2025-03-30 21:24:04 +0900

    [kotlin](코틀린 시큐리티 예제) build.gradle JPA 엔티티 세팅, yml 세팅

--- projects/kotlin/kt-security-example/build.gradle.kts
+++ projects/kotlin/kt-security-example/build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
+    val kotlinVersion = "1.9.25"
+
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
     id("org.springframework.boot") version "3.4.4"
     id("io.spring.dependency-management") version "1.1.7"
-    kotlin("plugin.jpa") version "1.9.25"
+    kotlin("plugin.jpa") version kotlinVersion
 }
 
 group = "com.example"
@@ -30,6 +32,16 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+allOpen {
+    // @Entity 어노테이션이 붙은 모든 클래스를 자동으로 'open'으로 설
+    annotation("jakarta.persistence.Entity")
+}
+
+noArg {
+    // 엔티티 클래스는 기본 생성자가 필요.
+    annotation("jakarta.persistence.Entity")
 }
 
 kotlin {
