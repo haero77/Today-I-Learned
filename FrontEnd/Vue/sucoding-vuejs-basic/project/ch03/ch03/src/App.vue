1002ef40430e72f53c0551da70851011785d925e
haero77 <shineecard5@gmail.com>
2025-08-23 22:51:14 +0900

    [Vue] sucoding vue.js 3.3 기본 문법 - 옵션스 API, 데이터 정의, 디렉티브

--- FrontEnd/Vue/sucoding-vuejs-basic/project/ch03/ch03/src/App.vue
+++ FrontEnd/Vue/sucoding-vuejs-basic/project/ch03/ch03/src/App.vue
@@ -1,11 +1,13 @@
 <template>
-
+  <div v-html="message"></div>
 </template>
 
 <script>
-
+export default {
+  data() {
+    return {
+      message: '<h1>Hello Vue.js!!</h1>'
+    }
+  },
+}
 </script>
-
-<style>
-
-</style>
